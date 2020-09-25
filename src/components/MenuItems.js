720c88cc702b9080a3be4d0663cd6e58494bf6e1
Madany Diallo <Madanyx@gmail.com>
2020-09-24 21:39:16 -0400

    init commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "01. About",
+    path: "/marketing",
+    cName: "dropdown-link",
+  },
+  {
+    title: "02. Experience",
+    path: "/consulting",
+    cName: "dropdown-link",
+  },
+  {
+    title: "03. Work",
+    path: "/design",
+    cName: "dropdown-link",
+  },
+  {
+    title: "04. Contact",
+    path: "/development",
+    cName: "dropdown-link",
+  },
+];
