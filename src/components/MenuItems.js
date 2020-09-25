e7971dd97b3ef4a4a060448ff72224cc71c38e03
Madany Diallo <Madanyx@gmail.com>
2020-09-24 23:10:14 -0400

    Update MenuItems.js

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,21 +1,21 @@
 export const MenuItems = [
   {
-    title: "01. About",
+    title: "01. Project",
     path: "/marketing",
     cName: "dropdown-link",
   },
   {
-    title: "02. Experience",
+    title: "02. Project",
     path: "/consulting",
     cName: "dropdown-link",
   },
   {
-    title: "03. Work",
+    title: "03. Project",
     path: "/design",
     cName: "dropdown-link",
   },
   {
-    title: "04. Contact",
+    title: "04. Project",
     path: "/development",
     cName: "dropdown-link",
   },
