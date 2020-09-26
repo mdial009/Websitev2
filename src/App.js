817b4b9c7e6e8e3a844f4404ca53021a402e972c
Madany Diallo <Madanyx@gmail.com>
2020-09-26 15:45:23 -0400

    Sections

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/pages/Experience";
+import Work from "./components/pages/Work";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
   <Route path="/Contact" component={Contact} />  */}
       <About />
       <Experience />
+      <Work />
       <Contact />
       <Footer />
     </Router>
---
+++ src/components/pages/Work.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App.css";
+
+export default function Work() {
+  return (
+    <>
+      <h1 className="Work">Work</h1>
+    </>
+  );
+}
