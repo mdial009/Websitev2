4d610c43e190f03a9975c814f944689130453a48
Madany Diallo <Madanyx@gmail.com>
2020-09-26 20:58:24 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     if (process.env.NODE_ENV !== "production") {
       return;
     }
-    fetch("https://api.github.com/repos/bchiang7/v4")
+    fetch("https://api.github.com/repos/mdial0097/websitev2")
       .then((response) => response.json())
       .then((json) => {
         const { stargazers_count, forks_count } = json;
