6077cc2ab7144ba2d296393ac728d8d36117e477
Madany Diallo <Madanyx@gmail.com>
2020-09-28 17:35:23 -0400

    updates

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
 import "./About.css";
+import MePic2 from "../../images/MePic2.JPG";
 
 export default function About() {
   return (
@@ -9,8 +10,20 @@ export default function About() {
         <div className="About-Container">
           <h1 className="madany">Hi, my name is</h1>
           <h2>Madany Diallo</h2>
-          <h3>Software Engineer</h3>
           <p>I'm a software engineer living in the Bronx.</p>
+          <div className="box">
+            <div class="card">
+              <div class="imgBx">
+                <img src={MePic2} alt={"Madany Diallo"}></img>
+              </div>
+              <div class="details">
+                <h2>
+                  Software Engineer
+                  <span>Ⓜ️🅰️🇩🅰️🇳🇾</span>
+                </h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "../../App.css";
+import MePic2 from "../../images/MePic2.JPG";
+import "./Experience.css";
 
 export default function Experience() {
   return (
     <>
-      <h1 className="Experience">Experience</h1>
+      <section className="hero-section">
+        <div className="card-grid">
+          <a className="cards" href="#">
+            <div className="card__background">
+              <img src={MePic2} alt={"Madany Diallo"}></img>
+              <div className="card__content">
+                <p className="card__category">sdds</p>
+                <h3 class="card__heading">sdsddsssdsdszsdsddsszds</h3>
+              </div>
+            </div>
+          </a>
+        </div>
+      </section>
     </>
   );
 }
