a30f6aa442ec6423b6e4ae6191212503eac5873b
Madany Diallo <Madanyx@gmail.com>
2020-09-27 21:55:17 -0400

    Big updates

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "../../App.css";
+import "./About.css";
 
 export default function About() {
   return (
     <>
-      <h1 className="About">About</h1>
+      <div className="About">
+        <div className="About-Container">
+          <h1 className="madany">Hi, my name is</h1>
+          <h2>Madany Diallo</h2>
+          <h3>Software Engineer</h3>
+          <p>I'm a software engineer living in the Bronx.</p>
+        </div>
+      </div>
     </>
   );
 }
