92900e233b0ab38f26b32a8652859fda4b9e290a
Madany Diallo <Madanyx@gmail.com>
2020-11-26 15:53:58 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import Experience from "./components/pages/Experience";
 import Work from "./components/pages/Work";
 
+
 function App() {
   return (
     <Router>
@@ -19,6 +20,7 @@ function App() {
       <Work />
       <Footer />
     </Router>
+    
   );
 }
 
