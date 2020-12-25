aaeca404d3001116e0791a3762ffbc5cbde4cf4f
Madany Diallo <Madanyx@gmail.com>
2020-12-25 00:10:00 -0500

    updates

--- src/components/sections/Archive.js
+++ src/components/sections/Archive.js
@@ -1,46 +1,67 @@
 import React from "react";
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid } from "@material-ui/data-grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: "56.25%", // 16:9
+    color: "#0096c7",
+
+
+  },
+}));
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
+  { field: "Year", headerName: "Year", width: 68 },
+  { field: "Title", headerName: "Title", width: 156 },
+  { field: "Made", headerName: "Made at", width: 94 },
+  { field: "Built", headerName: "Built with", width: 200 },
+  { field: "Link", headerName: "Link", width: 450 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    id: 1,
+    Year: 2020,
+    Title: "Discord Bot",
+    Made: " — ",
+    Built: "React·CSS·HTML",
+    Link: "https://github.com/mdial009/Discord-Bot",
+  },
+  {
+    id: 2,
+    Year: 2020,
+    Title: "Mailing-Client",
+    Made: " — ",
+    Built: "Python",
+    Link: "https://github.com/mdial009/Mailing-Client",
+  },
+  {
+    id: 3,
+    Year: 2020,
+    Title: "L3v3l3ditor",
+    Made: " — ",
+    Built: "Python·C#·Unity3D",
+    Link: "https://github.com/mdial009/L3v3l3ditor",
+  },
 ];
 
 export default function Archive() {
+  const classes = useStyles();
+
   return (
     <>
-       <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-    </div>
+      <h1>Archive</h1>
+      <span>A big list of things I’ve worked on</span>
+      <div style={{ height: 500, width: "auto" }}>
+        <DataGrid
+          className={classes.root}
+          rows={rows}
+          columns={columns}
+          pageSize={20}
+          checkboxSelection
+        />
+      </div>
     </>
   );
 }
-
