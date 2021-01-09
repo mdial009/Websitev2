c3f410628ec8b07a690218ed781aa9f273edc5c4
Madany Diallo <Madanyx@gmail.com>
2021-01-09 13:49:55 -0500

    update

--- src/components/sections/Archive.js
+++ src/components/sections/Archive.js
@@ -5,7 +5,6 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
     color: "#0096c7",
-
   },
 }));
 
@@ -41,6 +40,102 @@ const rows = [
     Built: "Python·C#·Unity3D",
     Link: "https://github.com/mdial009/L3v3l3ditor",
   },
+  {
+    id: 4,
+    Year: 2020,
+    Title: "Countdown",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 5,
+    Year: 2020,
+    Title: "Grocery",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 6,
+    Year: 2020,
+    Title: "Lorem",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 7,
+    Year: 2020,
+    Title: "Navbar",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 8,
+    Year: 2020,
+    Title: "Questions",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 9,
+    Year: 2020,
+    Title: "RandomColor",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 10,
+    Year: 2020,
+    Title: "Reviews",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 11,
+    Year: 2020,
+    Title: "Scroll",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 12,
+    Year: 2020,
+    Title: "Sidebars",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 13,
+    Year: 2020,
+    Title: "Slider",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 14,
+    Year: 2020,
+    Title: "Tabs",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
+  {
+    id: 15,
+    Year: 2020,
+    Title: "Video",
+    Made: " — ",
+    Built: "Javascript·HTML·CSS",
+    Link: "https://github.com/mdial009/Javascript",
+  },
 ];
 
 export default function Archive() {
@@ -50,7 +145,7 @@ export default function Archive() {
     <>
       <h1>Archive</h1>
       <span>A big list of things I’ve worked on</span>
-      <div style={{ height: 500, width: "auto" }}>
+      <div style={{ height: 900, width: "auto" }}>
         <DataGrid
           className={classes.root}
           rows={rows}
